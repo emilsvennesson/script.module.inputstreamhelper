a6fafb9bb0a1193729eb82d15b4c1f2df963c6b9
horstle <horstle.seppl@t-online.de>
2019-08-19 23:05:30 +0200

    Fix lint except "protected-access".

--- lib/run_manually.py
+++ lib/run_manually.py
@@ -1,6 +1,7 @@
+# -*- coding: utf-8 -*-
+''' Used to run manually or force a reinstall '''
 import os
 import sys
-import xbmc
 import xbmcaddon
 import xbmcgui
 import inputstreamhelper
@@ -11,9 +12,9 @@ LANGUAGE = ADDON.getLocalizedString
 is_helper = inputstreamhelper.Helper('mpd', drm='com.widevine.alpha')
 
 if len(sys.argv) == 2:
-	if sys.argv[1] == "force_reinstall" and os.path.lexists(is_helper._widevine_config_path()):
-		os.remove(is_helper._widevine_config_path())
+    if sys.argv[1] == "force_reinstall" and os.path.lexists(is_helper._widevine_config_path()):
+        os.remove(is_helper._widevine_config_path())
 
 if is_helper.check_inputstream(ignore_disabled=True):
-	dialog = xbmcgui.Dialog()
-	dialog.ok(LANGUAGE(30037), LANGUAGE(30003))
+    dialog = xbmcgui.Dialog()
+    dialog.ok(LANGUAGE(30037), LANGUAGE(30003))

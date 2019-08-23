b7172ddfe28c598658fe2fd2b8b260bf337e8509
Media Minister <mediaminister@gmail.com>
2019-08-23 09:07:56 +0200

    fix unicode error

--- lib/inputstreamhelper.py
+++ lib/inputstreamhelper.py
@@ -734,7 +734,7 @@ class Helper:
 
     def _extract_widevine_from_img(self):
         """Extracts the Widevine CDM binary from the mounted Chrome OS image."""
-        for root, dirs, files in os.walk(str(self._mnt_path())):  # pylint: disable=unused-variable
+        for root, dirs, files in os.walk(self._mnt_path()):  # pylint: disable=unused-variable
             for filename in files:
                 if filename == 'libwidevinecdm.so':
                     cdm_path = os.path.join(root, filename)

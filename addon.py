2c1258bc88c9ce49345e698fdc2ddf2c4bc2bfd5
Media Minister <mediaminister@gmail.com>
2019-09-01 12:05:50 +0200

    implement api using runscript

--- addon.py
+++ addon.py
@@ -3,46 +3,41 @@
 
 from __future__ import absolute_import, division, unicode_literals
 import sys
-from routing import Plugin
-from inputstreamhelper import ADDON, Helper
-
-# NOTE: Work around an issue in script.module.routing
-#       https://github.com/tamland/kodi-plugin-routing/pull/16
-if len(sys.argv) < 1:
-    sys.argv.append('addon.py')
-if len(sys.argv) < 2:
-    sys.argv.append('-1')
-
-plugin = Plugin()
+from inputstreamhelper import ADDON, Helper, log
 
 
-@plugin.route('addon.py')  # This is the entry point from the addon menu
-@plugin.route('/')
-@plugin.route('/settings')
-def settings():
-    ''' Entry point to open the plugin settings '''
-    ADDON.openSettings()
+def run(params):
+    ''' Route to API method '''
+    if 2 <= len(params) <= 4:
+        if params[1] == 'widevine_install':
+            widevine_install()
+        elif params[1] == 'widevine_remove':
+            widevine_remove()
+        elif params[1] == 'check_inputstream':
+            if len(params) == 3:
+                check_inputstream(params[2])
+            elif len(params) == 4:
+                check_inputstream(params[2], drm=params[3])
+    elif len(params) > 4:
+        log('invalid API call, too many parameters')
+    else:
+        ADDON.openSettings()
 
 
-@plugin.route('/check/<protocol>')
-@plugin.route('/check/<protocol>/<drm>')
 def check_inputstream(protocol, drm=None):
     ''' The API interface to check inputstream '''
     Helper(protocol, drm=drm).check_inputstream()
 
 
-@plugin.route('/widevine/install')
-@plugin.route('/widevine/install/latest')
 def widevine_install():
     ''' The API interface to install Widevine CDM '''
     Helper('mpd', drm='widevine').install_widevine()
 
 
-@plugin.route('/widevine/remove')
 def widevine_remove():
     ''' The API interface to remove Widevine CDMs '''
     Helper('mpd', drm='widevine').remove_widevine()
 
 
 if __name__ == '__main__':
-    plugin.run(sys.argv)
+    run(sys.argv)

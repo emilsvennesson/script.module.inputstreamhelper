f06491091a3696f8d2a6d340359b576ecfd869bf
Dag Wieers <dag@wieers.com>
2019-11-14 23:02:56 +0100

    Improve exception-handling
    Only try the statement we expect to fail.
    Move everything else out of the try-block.

--- lib/inputstreamhelper/__init__.py
+++ lib/inputstreamhelper/__init__.py
@@ -277,21 +277,24 @@ class Helper:
 
     def _run_cmd(self, cmd, sudo=False, shell=False):
         ''' Run subprocess command and return if it succeeds as a bool '''
+        from .unicodehelper import to_unicode
         import subprocess
         output = ''
         success = False
         if sudo and os.getuid() != 0 and self._cmd_exists('sudo'):
             cmd.insert(0, 'sudo')
+
         try:
-            from .unicodehelper import to_unicode
             output = to_unicode(subprocess.check_output(cmd, shell=shell, stderr=subprocess.STDOUT))
-            success = True
-            log('{cmd} cmd executed successfully.', cmd=cmd)
         except subprocess.CalledProcessError as error:
             output = error.output
             log('{cmd} cmd failed.', cmd=cmd)
         except OSError as error:
             log('{cmd} cmd doesn\'t exist. {error}', cmd=cmd, error=error)
+        else:
+            success = True
+            log('{cmd} cmd executed successfully.', cmd=cmd)
+
         if output.rstrip():
             log('{cmd} cmd output:\n{output}', cmd=cmd, output=output)
         if 'sudo' in cmd:

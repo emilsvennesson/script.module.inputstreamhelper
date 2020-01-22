bf5c63b8d0d5359922e07749cec86ec10d9181e2
Dag Wieers <dag@wieers.com>
2020-01-22 23:28:23 +0100

    Port kodiutils fixes to inputstreamhelper
    This is a port from the VRT NU kodiutils:
    - Fix a reported issue related to enabling inputstream.adaptive
    - Implement type-specific get_setting() and set_setting()
    - Improve get_proxies() implementation
    - Implement key-specific get_addon_info() calls
    - Change order of functions to ease porting
    - Rename unicodehelper.py to utils.py

--- test/xbmcvfs.py
+++ test/xbmcvfs.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 # Copyright: (c) 2019, Dag Wieers (@dagwieers) <dag@wieers.com>
 # GNU General Public License v3.0 (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
-''' This file implements the Kodi xbmcvfs module, either using stubs or alternative functionality '''
+"""This file implements the Kodi xbmcvfs module, either using stubs or alternative functionality"""
 
-# pylint: disable=invalid-name
+# pylint: disable=invalid-name,too-few-public-methods
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 import os
@@ -11,34 +11,34 @@ from shutil import copyfile
 
 
 def File(path, flags='r'):
-    ''' A reimplementation of the xbmcvfs File() function '''
+    """A reimplementation of the xbmcvfs File() function"""
     return open(path, flags)
 
 
 def Stat(path):
-    ''' A reimplementation of the xbmcvfs Stat() function '''
+    """A reimplementation of the xbmcvfs Stat() function"""
 
-    class stat:  # pylint: disable=too-few-public-methods
-        ''' A reimplementation of the xbmcvfs stat class '''
+    class stat:
+        """A reimplementation of the xbmcvfs stat class"""
 
         def __init__(self, path):
-            ''' The constructor xbmcvfs stat class '''
+            """The constructor xbmcvfs stat class"""
             self._stat = os.stat(path)
 
         def st_mtime(self):
-            ''' The xbmcvfs stat class st_mtime method '''
+            """The xbmcvfs stat class st_mtime method"""
             return self._stat.st_mtime
 
     return stat(path)
 
 
 def copy(src, dst):
-    ''' A reimplementation of the xbmcvfs mkdir() function '''
+    """A reimplementation of the xbmcvfs mkdir() function"""
     return copyfile(src, dst) == dst
 
 
 def delete(path):
-    ''' A reimplementation of the xbmcvfs delete() function '''
+    """A reimplementation of the xbmcvfs delete() function"""
     try:
         os.remove(path)
     except OSError:
@@ -46,12 +46,12 @@ def delete(path):
 
 
 def exists(path):
-    ''' A reimplementation of the xbmcvfs exists() function '''
+    """A reimplementation of the xbmcvfs exists() function"""
     return os.path.exists(path)
 
 
 def listdir(path):
-    ''' A reimplementation of the xbmcvfs listdir() function '''
+    """A reimplementation of the xbmcvfs listdir() function"""
     files = []
     dirs = []
     if not exists(path):
@@ -66,15 +66,15 @@ def listdir(path):
 
 
 def mkdir(path):
-    ''' A reimplementation of the xbmcvfs mkdir() function '''
+    """A reimplementation of the xbmcvfs mkdir() function"""
     return os.mkdir(path)
 
 
 def mkdirs(path):
-    ''' A reimplementation of the xbmcvfs mkdirs() function '''
+    """A reimplementation of the xbmcvfs mkdirs() function"""
     return os.makedirs(path)
 
 
 def rmdir(path):
-    ''' A reimplementation of the xbmcvfs rmdir() function '''
+    """A reimplementation of the xbmcvfs rmdir() function"""
     return os.rmdir(path)

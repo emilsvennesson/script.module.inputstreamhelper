43b45fdd72c39d12b5edb75d03cb1b58b793ffb8
mediaminister <mediaminister@gmail.com>
2020-03-04 08:16:43 +0100

    Use explicit relative imports instead of sys.path.append

--- default.py
+++ default.py
@@ -3,18 +3,6 @@
 
 from __future__ import absolute_import, division, unicode_literals
 import sys
-import os.path
-from xbmc import translatePath
-from xbmcaddon import Addon
-
-
-def to_unicode(text, encoding='utf-8'):
-    ''' Force text to unicode '''
-    return text.decode(encoding) if isinstance(text, bytes) else text
-
-
-# NOTE: This is required so our RunScript interface can import our own inputstreamhelper package
-sys.path.append(os.path.join(to_unicode(translatePath(Addon().getAddonInfo('path'))), 'lib'))
-from inputstreamhelper.api import run  # noqa: E402
+from lib.inputstreamhelper.api import run
 
 run(sys.argv)

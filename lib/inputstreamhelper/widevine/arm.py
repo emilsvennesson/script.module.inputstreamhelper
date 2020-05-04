a05c4e39c691b8a9ca58168b7c2bb13ec73a3f95
Thorsten <horstle.seppl@t-online.de>
2020-05-05 00:35:21 +0200

    Extract the lib directly from the zip.

--- lib/inputstreamhelper/widevine/arm.py
+++ lib/inputstreamhelper/widevine/arm.py
@@ -4,13 +4,14 @@
 
 from __future__ import absolute_import, division, unicode_literals
 import os
+import json
 from time import time
-from struct import unpack, calcsize
 
 from .. import config
 from ..kodiutils import browsesingle, copy, exists, localize, log, mkdir, ok_dialog, open_file, progress_dialog, yesno_dialog
 from ..utils import cmd_exists, diskspace, http_download, http_get, run_cmd, sizeof_fmt, store, system_os, temp_path, update_temp_path
 from ..unicodes import compat_path, to_unicode
+from .arm_chromeos import ChromeOSImage
 
 
 def mnt_path():
@@ -20,52 +21,6 @@ def mnt_path():
         mkdir(mount_path)
 
     return mount_path
-
-
-def gpt_header(bytestream):
-    """Returns the needed parts of the GPT header, can be easily expanded if necessary"""
-    header_fmt = '<8s4sII4x4Q16sQ3I'
-    header_size = calcsize(header_fmt)
-
-    # GPT Header entries: signature, revision, header_size, header_crc32, (reserved 4x skipped,) current_lba, backup_lba,
-    #                     first_usable_lba, last_usable_lba, disk_guid, start_lba_part_entries, num_part_entries,
-    #                     size_part_entry, crc32_part_entries
-    _, _, _, _, _, _, _, _, _, start_lba_part_entries, num_part_entries, size_part_entry, _ = unpack(header_fmt, bytestream.read(header_size))
-    bytestream.read(config.CHROMEOS_BLOCK_SIZE - header_size)  # go to LBA 2
-
-    return (start_lba_part_entries, num_part_entries, size_part_entry)
-
-
-def chromeos_offset(bytestream):
-    """Calculate the Chrome OS losetup start offset"""
-    lba_size = config.CHROMEOS_BLOCK_SIZE
-    part_format = '<16s16sQQQ72s'
-    bytestream.read(lba_size)  # skip MBR
-    entries_start, entries_num, entry_size = gpt_header(bytestream)
-
-    if not entries_start == 2:
-        if entries_start < 2:
-            log(4, 'GPT partition entries start too early')
-            return 0
-        bytestream.read((entries_start - 2) * lba_size)
-
-    if not calcsize(part_format) == entry_size:
-        log(4, 'Partition table entries are not 128 bytes long')
-        return 0
-
-    for index in range(1, entries_num+1):  # pylint: disable=unused-variable
-        #Entry: type_guid, unique_guid, first_lba, last_lba, attr_flags, part_name
-        _, _, first_lba, _, _, part_name = unpack(part_format, bytestream.read(entry_size))
-        part_name = part_name.decode('utf-16').strip('\x00')
-        if part_name == 'ROOT-A':
-            offset = first_lba * lba_size
-            break
-
-    if not offset:
-        log(4, 'Failed to calculate losetup offset.')
-        return 0
-
-    return offset
 
 
 def check_loop():
@@ -95,8 +50,7 @@ def set_loop_dev():
 
 def losetup(bin_path):
     """Setup Chrome OS loop device."""
-    with open(bin_path, 'rb') as cos_image:
-        cos_offset = str(chromeos_offset(cos_image))
+    cos_offset = str(ChromeOSImage(bin_path).chromeos_offset())
 
     cmd = ['losetup', '-o', cos_offset, store('loop_dev'), bin_path]
     output = run_cmd(cmd, sudo=True)
@@ -184,15 +138,72 @@ def chromeos_config():
     return devices
 
 
-def install_widevine_arm(backup_path):  # pylint: disable=too-many-statements
+def install_widevine_arm(backup_path):
     """Installs Widevine CDM on ARM-based architectures."""
+    devices = chromeos_config()
+    arm_device = select_best_chromeos_image(devices)
+    if arm_device is None:
+        log(4, 'We could not find an ARM device in the Chrome OS recovery.conf')
+        ok_dialog(localize(30004), localize(30005))
+        return False
+    # Estimated required disk space: takes into account an extra 20 MiB buffer
+    required_diskspace = 20971520 + int(arm_device['zipfilesize'])
+    if yesno_dialog(localize(30001),  # Due to distributing issues, this takes a long time
+                    localize(30006, diskspace=sizeof_fmt(required_diskspace))):
+        if system_os() != 'Linux':
+            ok_dialog(localize(30004), localize(30019, os=system_os()))
+            return False
+
+        while required_diskspace >= diskspace():
+            if yesno_dialog(localize(30004), localize(30055)):  # Not enough space, alternative path?
+                update_temp_path(browsesingle(3, localize(30909), 'files'))  # Temporary path
+                continue
+
+            ok_dialog(localize(30004),  # Not enough free disk space
+                      localize(30018, diskspace=sizeof_fmt(required_diskspace)))
+            return False
+
+        url = arm_device['url']
+        downloaded = http_download(url, message=localize(30022), checksum=arm_device['sha1'], hash_alg='sha1', dl_size=int(arm_device['zipfilesize']))  # Downloading the recovery image
+        if downloaded:
+            progress = progress_dialog()
+            progress.create(heading=localize(30043), message=localize(30044))  # Extracting Widevine CDM
+
+            extracted = ChromeOSImage(store('download_path')).extract_file(
+                filename=config.WIDEVINE_CDM_FILENAME[system_os()],
+                extract_path=os.path.join(backup_path, arm_device['version']),
+                progress=progress)
+
+            if not extracted:
+                log(3, 'Directly extracting widevine from the zip failed, using legacy method.')
+                if yesno_dialog(heading=localize(30004),
+                                message='{line1}\n{line2}\n{line3}'.format(
+                                    line1=localize(30016),
+                                    line2=localize(30830, url=config.ISSUE_URL),
+                                    line3=localize(30059))
+                               ):
+                    return install_wv_arm_legacy(backup_path)
+                return False
+
+
+            json_file = os.path.join(backup_path, arm_device['version'], os.path.basename(config.CHROMEOS_RECOVERY_URL) + '.json')
+            with open_file(json_file, 'w') as config_file:
+                config_file.write(json.dumps(devices, indent=4))
+
+            return (progress, arm_device['version'])
+
+    return False
+
+
+def install_wv_arm_legacy(backup_path):
+    """Legacy method to install Widevine CDM on ARM-based architectures."""
     root_cmds = ['mount', 'umount', 'losetup', 'modprobe']
     devices = chromeos_config()
     arm_device = select_best_chromeos_image(devices)
     if arm_device is None:
         log(4, 'We could not find an ARM device in the Chrome OS recovery.conf')
         ok_dialog(localize(30004), localize(30005))
-        return ''
+        return False
     # Estimated required disk space: takes into account an extra 20 MiB buffer
     required_diskspace = 20971520 + int(arm_device['zipfilesize']) + int(arm_device['filesize'])
     if yesno_dialog(localize(30001),  # Due to distributing issues, this takes a long time
@@ -228,55 +239,52 @@ def install_widevine_arm(backup_path):  # pylint: disable=too-many-statements
             return False
 
         url = arm_device['url']
-        downloaded = http_download(url, message=localize(30022), checksum=arm_device['sha1'], hash_alg='sha1', dl_size=int(arm_device['zipfilesize']))  # Downloading the recovery image
-        if downloaded:
-            progress = progress_dialog()
-            progress.create(heading=localize(30043), message=localize(30044))  # Extracting Widevine CDM
-            bin_filename = url.split('/')[-1].replace('.zip', '')
-            bin_path = compat_path(os.path.join(temp_path(), bin_filename))
-            starttime = time()
+        progress = progress_dialog()
+        progress.create(heading=localize(30043), message=localize(30044))  # Extracting Widevine CDM
+        bin_filename = url.split('/')[-1].replace('.zip', '')
+        bin_path = compat_path(os.path.join(temp_path(), bin_filename))
+        starttime = time()
 
-            progress.update(
-                0,
-                message='{line1}\n{line2}\n{line3}'.format(
-                    line1=localize(30045),  # Uncompressing image
-                    line2=localize(30058, mins=0, secs=0),  # Time remaining
-                    line3=localize(30047))  # Please do not interrupt this process
-            )
+        progress.update(
+            0,
+            message='{line1}\n{line2}\n{line3}'.format(
+                line1=localize(30045),  # Uncompressing image
+                line2=localize(30058, mins=0, secs=0),  # Time remaining
+                line3=localize(30047))  # Please do not interrupt this process
+        )
 
-            from zipfile import ZipFile
+        from zipfile import ZipFile
 
-            with ZipFile(compat_path(store('download_path'))) as zip_obj:
-                bin_size = zip_obj.getinfo(bin_filename).file_size
-                chunksize = 1024**2
+        with ZipFile(compat_path(store('download_path'))) as zip_obj:
+            bin_size = zip_obj.getinfo(bin_filename).file_size
+            chunksize = 1024**2
 
-                with zip_obj.open(bin_filename) as member:
-                    with open(bin_path, 'wb') as bin_file:
-                        bytes_to_read = bin_size
-                        while bytes_to_read > 0:
-                            chunk = member.read(chunksize)
-                            bytes_to_read -= chunksize
-                            bin_file.write(chunk)
-                            percent = 100 * (1 - bytes_to_read / bin_size) - 5
-                            time_left = int(bytes_to_read * (time() - starttime) / (bin_size - bytes_to_read))
-                            progress.update(
-                                int(percent),
-                                message='{line1}\n{line2}\n{line3}'.format(
-                                    line1=localize(30045),  # Uncompressing image
-                                    line2=localize(30058, mins=time_left // 60, secs=time_left % 60),  # Time remaining
-                                    line3=localize(30047))  # Please do not interrupt this process
-                            )
+            with zip_obj.open(bin_filename) as member:
+                with open(bin_path, 'wb') as bin_file:
+                    bytes_to_read = bin_size
+                    while bytes_to_read > 0:
+                        chunk = member.read(chunksize)
+                        bytes_to_read -= chunksize
+                        bin_file.write(chunk)
+                        percent = 100 * (1 - bytes_to_read / bin_size) - 5
+                        time_left = int(bytes_to_read * (time() - starttime) / (bin_size - bytes_to_read))
+                        progress.update(
+                            int(percent),
+                            message='{line1}\n{line2}\n{line3}'.format(
+                                line1=localize(30045),  # Uncompressing image
+                                line2=localize(30058, mins=time_left // 60, secs=time_left % 60),  # Time remaining
+                                line3=localize(30047))  # Please do not interrupt this process
+                        )
 
-            if check_loop() and set_loop_dev() and losetup(bin_path) and mnt_loop_dev():
-                import json
-                progress.update(96, message=localize(30048))  # Extracting Widevine CDM
-                extract_widevine_from_img(os.path.join(backup_path, arm_device['version']))
-                json_file = os.path.join(backup_path, arm_device['version'], os.path.basename(config.CHROMEOS_RECOVERY_URL) + '.json')
-                with open_file(json_file, 'w') as config_file:
-                    config_file.write(json.dumps(devices, indent=4))
+        if check_loop() and set_loop_dev() and losetup(bin_path) and mnt_loop_dev():
+            progress.update(96, message=localize(30048))  # Extracting Widevine CDM
+            extract_widevine_from_img(os.path.join(backup_path, arm_device['version']))
+            json_file = os.path.join(backup_path, arm_device['version'], os.path.basename(config.CHROMEOS_RECOVERY_URL) + '.json')
+            with open_file(json_file, 'w') as config_file:
+                config_file.write(json.dumps(devices, indent=4))
 
-                return (progress, arm_device['version'])
-            progress.close()
+            return (progress, arm_device['version'])
+        progress.close()
 
     return False
 

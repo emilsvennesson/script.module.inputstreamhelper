7a8f04095b429e65a898ceb5bf7df37aad7ec634
Thorsten <horstle.seppl@t-online.de>
2020-05-24 12:53:57 +0200

    Wait before creating progress dialog

--- lib/inputstreamhelper/kodiutils.py
+++ lib/inputstreamhelper/kodiutils.py
@@ -19,6 +19,7 @@ class progress_dialog(DialogProgress, object):  # pylint: disable=invalid-name,u
     def create(self, heading, message=''):  # pylint: disable=arguments-differ
         """Create and show a progress dialog"""
         if kodi_version_major() < 19:
+            xbmc.sleep(400)  # Wait for previous dialog to close, else inherits lines
             lines = message.split('\n', 2)
             line1, line2, line3 = (lines + [None] * (3 - len(lines)))
             return super(progress_dialog, self).create(heading, line1=line1, line2=line2, line3=line3)

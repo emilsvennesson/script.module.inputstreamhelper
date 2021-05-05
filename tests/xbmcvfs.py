5ed9c8e3439786a7841e1c785d5ff5f097cde07b
mediaminister <45148099+mediaminister@users.noreply.github.com>
2021-05-05 13:32:45 +0200

    pylint fix (#435)

--- tests/xbmcvfs.py
+++ tests/xbmcvfs.py
@@ -12,7 +12,7 @@ from shutil import copyfile
 
 def File(path, flags='r'):
     """A reimplementation of the xbmcvfs File() function"""
-    return open(path, flags)
+    return open(path, flags)  # pylint: disable=consider-using-with
 
 
 def Stat(path):

1629c303ab26aed22a32a3d9528f09ee6f63512d
mediaminister <45148099+mediaminister@users.noreply.github.com>
2025-09-25 12:34:40 +0200

    Fix installing Widevine CDM on Windows 32-bit (#612)

---
+++ tests/test_ishelper_windows_x86.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+# GNU General Public License v3.0 (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+# pylint: disable=missing-docstring
+
+import unittest
+
+import inputstreamhelper
+from test_utils import delete_cached, cleanup
+
+xbmc = __import__('xbmc')
+xbmcaddon = __import__('xbmcaddon')
+xbmcgui = __import__('xbmcgui')
+xbmcvfs = __import__('xbmcvfs')
+
+
+class WindowsX86Tests(unittest.TestCase):
+
+    def setUp(self):
+        delete_cached()
+        cleanup()
+        inputstreamhelper.system_os = lambda: 'Windows'
+        inputstreamhelper.widevine.widevine.system_os = lambda: 'Windows'
+        inputstreamhelper.widevine.repo.system_os = lambda: 'Windows'
+        inputstreamhelper.arch = lambda: 'x86'
+        inputstreamhelper.widevine.widevine.arch = lambda: 'x86'
+        inputstreamhelper.widevine.repo.arch = lambda: 'x86'
+
+    def test_check_inputstream_mpd(self):
+        is_helper = inputstreamhelper.Helper('mpd', drm='com.widevine.alpha')
+        is_helper.remove_widevine()
+        is_installed = is_helper.check_inputstream()
+        self.assertTrue(is_installed, True)
+
+    def test_check_inputstream_hls_again(self):
+        is_helper = inputstreamhelper.Helper('hls', drm='com.widevine.alpha')
+        is_installed = is_helper.check_inputstream()
+        self.assertTrue(is_installed, True)
+
+    def test_check_inputstream_rtmp(self):
+        is_helper = inputstreamhelper.Helper('rtmp')
+        is_installed = is_helper.check_inputstream()
+        self.assertTrue(is_installed, True)
+
+    def test_check_inputstream_disabled(self):
+        is_helper = inputstreamhelper.Helper('mpd', drm='com.widevine.alpha')
+        is_helper.disable()
+        is_installed = is_helper.check_inputstream()
+        is_helper.enable()
+        self.assertTrue(is_installed, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
